0d5461963d94aa7506dba4185ecdf97c180a430e
Greg Marsella <gkmarsella@gmail.com>
2016-12-01 12:23:03 -0800

    Changed APIs

--- app.py
+++ app.py
@@ -3,6 +3,21 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_modus import Modus
 import os
 import pandas as pd
+import requests
+
+
+
+# Search Products
+
+
+
+
+
+
+
+
+
+
 
 
 # getting data from excel sheet
@@ -19,38 +34,76 @@ additive_list = [add for add in ad]
 app = Flask(__name__)
 api = Modus(app)
 
-
-
+food_essentials_key = app.config['FOOD_ESSENTIALS_KEY'] = os.environ.get('FOOD_ESSENTIALS_KEY')
 
 key = app.config['KEY'] = os.environ.get('KEY')
 secret = app.config['SECRET'] = os.environ.get('SECRET')
 
 
-@app.route('/search', methods=[ "GET"])
+@app.route('/', methods=[ "GET"])
 def search():
     return render_template("search.html")
 
 
 # show list of food items by name
+# --------------When API works -------------
+# @app.route('/results', methods=["GET"])
+# def results():
+
+#     productData = products.search(request.args.get('search-food')).data()
+
+#     list1 = [product.get('category') for product in productData]
+#     categories = list(set(list1))
+
+#     # to get ingredients 
+#     for product in productData:
+#         id = product['factual_id']
+#         loweredIngredients = f.get_row('products-cpg-nutrition', id).get('ingredients')
+#         if loweredIngredients:
+#             product['ingredients'] = [x.upper() for x in loweredIngredients]
+
+
+#     ad = table["additive"]
+#     additive_list = [add for add in ad]
+
+#     return render_template("results.html", products=productData, categories=categories, additive_list=additive_list)
+
+
+# -------------testing for when API is down-----------
+# @app.route('/results', methods=["GET"])
+# def results():
+
+#     productData = products.search(request.args.get('search-food')).data()
+
+#     list1 = [product.get('category') for product in productData]
+#     categories = list(set(list1))
+
+#     # to get ingredients 
+#     for product in productData:
+#         id = product['factual_id']
+#         loweredIngredients = f.get_row('products-cpg-nutrition', id).get('ingredients')
+#         if loweredIngredients:
+#             product['ingredients'] = [x.upper() for x in loweredIngredients]
+
+
+#     ad = table["additive"]
+#     additive_list = [add for add in ad]
+
+#     return render_template("results.html", products=productData, categories=categories, additive_list=additive_list)
+def get_sid():
+    sid_dict = {"api_key": food_essentials_key, "f": "json", "v": "2.00"}
+    sid = requests.get("http://api.foodessentials.com/createsession", params=sid_dict).json()["session_id"]
+    return sid
+
 @app.route('/results', methods=["GET"])
 def results():
+    # new session every time
+    sid = get_sid()
 
-    productData = products.search(request.args.get('search-food')).data()
+    search_dict = {"q": request.args.get('search-food').lower(), "n": 100, "sid": sid, "s": 1, "f": "json", "v": "2.00", "api_key": food_essentials_key}
+    search = requests.get("http://api.foodessentials.com/searchprods", params=search_dict).json()
+    return render_template("results.html", search=search)
 
-    list1 = [product.get('category') for product in productData]
-    categories = list(set(list1))
-
-    # to get ingredients 
-    for product in productData:
-        id = product['factual_id']
-        loweredIngredients = f.get_row('products-cpg-nutrition', id).get('ingredients')
-        if loweredIngredients:
-            product['ingredients'] = [x.upper() for x in loweredIngredients]
-
-
-    ad = table["additive"]
-    additive_list = [add for add in ad]
-    return render_template("results.html", products=productData, categories=categories, additive_list=additive_list)
 
 @app.route('/ingredients', methods=["GET"])
 def ingredients():
@@ -116,6 +169,9 @@ category_list = ['Alcoholic Beverages', 'Baby Food', 'Baking Ingredients', 'Baki
                  'Weight Loss Products & Supplements', 'Wheat Flours & Meals', 'Yogurt'] 
 #remove?: food storage, gift sets, sexual wellness, smoking cessation, vitamins & supplements, weight loss products & supplements
 
+
+
+
 if __name__ == '__main__':
     app.run(debug=True,port=3000)
 
@@ -127,4 +183,7 @@ if __name__ == '__main__':
 #                 <p>
 #                     <a  class="foodname">{{product['product_name']}}</a>
 #                 </p>            
-#             </li> -->    
+#             </li> -->   
+
+
+
